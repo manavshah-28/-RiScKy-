02ae3e4dbf46eb88f72c7d66ce770f2e8e2e70cc
manavshah-28 <manavbshah28@gmail.com>
2024-01-19 22:09:07 +0530

    step 5 complete

--- src/Single_Cycle_TOP.v
+++ src/Single_Cycle_TOP.v
@@ -4,6 +4,7 @@
 `include "extend.v"
 `include "alu.v"
 `include "control_unit_Top.v"
+`include "data_mempry.v"
 
 module Single_Cycle_TOP(clk,rst);
 
@@ -12,7 +13,9 @@ wire [31:0] PC_Top;
 wire [31:0] RD_Instr;
 wire [31:0]RD1_Top;
 wire [31:0]ImmExt_top;
-
+wire [31:0]ALUResult;
+wire regwrite;
+wire ReadData;
 Prog_Count PC(.clk(clk),
               .rst(rst),
               .PC(PC_Top),
@@ -24,11 +27,11 @@ Instruction_memory Instr_mem(.rst(rst),
 
 regsiter_file regfile(.clk(clk),
                       .rst(rst),
-                      .WE3(),
-                      .WD3(),
+                      .WE3(RegWrite),
+                      .WD3(ReadData),
                       .A1(RD_Instr[19:15]), // giving rs1 to A1
                       .A2(),
-                      .A3(),
+                      .A3(RD_Instr[11:7]),
                       .RD1(RD1_Top),
                       .RD2());
 
@@ -38,14 +41,14 @@ extend extend(.instr(RD_Instr),
 ALU alu(.A(RD1_Top),
         .B(ImmExt_top),
         .ALUControl(), 
-        .Result(),
+        .Result(ALUResult),
         .V(),
         .C(),
         .Z(),
         .N());
         
 Control_unit_top control_top(.Op(RD_Instr[6:0]),
-                             .RegWrite(),
+                             .RegWrite(regwrite),
                              .ImmSrc(),
                              .ALUSrc(),
                              .MemWrite(),
@@ -54,6 +57,12 @@ Control_unit_top control_top(.Op(RD_Instr[6:0]),
                              .funct3(RD_Instr[14:12]),
                              .funct7(),
                              .ALUControl());
+
+data_memory data_memr(.clk(clk),
+                      .A(ALUResult),
+                      .WD(),
+                      .WE(regwrite),
+                      .RD(ReadData))
 
 
 endmodule
