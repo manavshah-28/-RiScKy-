9c979c5b50f4072d32f0aa70a713a93f098ed1d9
manavshah-28 <manavbshah28@gmail.com>
2024-01-19 10:47:04 +0530

    added slt to ALU

--- individual module tests/ALU/alu.v
+++ individual module tests/ALU/alu.v
@@ -17,7 +17,8 @@ wire [31:0] mux_1;
 wire [31:0] sum;
 wire [31:0] mux_2;
 wire cout;
-
+// slt takes only the most signifiacant bit
+wire [31:0]slt; // set less than (N-1 zero extended to 32 bits).
 //Logical Operations
 assign a_and_b = A & B;
 assign a_or_b = A | B;
@@ -25,10 +26,14 @@ assign not_b = ~B;
 
 assign mux_1 = (ALUControl[0] == 1'b0) ? B : not_b;
 assign {cout,sum} = A + mux_1 + ALUControl[0];
+assign slt = {31'b0000000000000000000000000000000,sum[31]};
+
 // cout is concatenated with sum. 
-assign mux_2 = (ALUControl[1:0] == 2'b00) ? sum :
-               (ALUControl[1:0] == 2'b01) ? sum :
-               (ALUControl[1:0] == 2'b10) ? a_and_b : a_or_b;
+assign mux_2 = (ALUControl[2:0] == 2'b000) ? sum :
+               (ALUControl[2:0] == 2'b001) ? sum :
+               (ALUControl[2:0] == 2'b010) ? a_and_b : 
+               (ALUControl[2:0] == 2'b011) ? a_or_b : 
+               (ALUControl[2:0] == 2'b101) ? slt : 32'h00000000;
 
 assign Result = mux_2;
 
