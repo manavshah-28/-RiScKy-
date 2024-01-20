d59814d361fcd83e50d14eec55dc71854f109b62
manavshah-28 <manavbshah28@gmail.com>
2024-01-20 20:16:39 +0530

    instantiated regfile in top design

--- single_cycle_rv32i/RV32_top.v
+++ single_cycle_rv32i/RV32_top.v
@@ -1,6 +1,7 @@
 `include "PC.v"
 `include "PC_adder.v"
 `include "Instruction_memory.v"
+`include "Regfile.v"
 
 module RV32_top(clk,rst);
 
@@ -25,8 +26,15 @@ Instruction_memory Instruction_memory(.rst(rst),
                                       .A(PC_Top),
                                       .RD(Instr));
 
-/*always @(posedge clk)begin
-   instr = Instr;
-end   */          
+Regfile regfile(.clk(clk),
+                .rst(rst),
+                .WE(),
+                .AddD(Instr[11:7]), // [11:7] = rd 
+                .DataD(),
+                .AddA(Instr[19:15]), // [19:15] = rs1
+                .DataA(),
+                .AddB(Instr[24:20]), // [24:20] = rs2
+                .DataB());
+
 
 endmodule
