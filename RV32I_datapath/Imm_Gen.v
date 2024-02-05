51b8b9752adb0586909bb2d0d8a5d2a52a9d5221
manavshah-28 <manavbshah28@gmail.com>
2024-02-05 11:34:58 +0530

    another problem was with the zero position immediate bit for Branch instructions which is now solved, final tests for branch instructions are remaining.

--- RV32I_datapath/Imm_Gen.v
+++ RV32I_datapath/Imm_Gen.v
@@ -17,8 +17,8 @@ assign immediate = (ImmSel == 3'b001 & Instr[31] == 1'b0) ? {{20{1'b0}},Instr[31
                    (ImmSel == 3'b001 & Instr[31] == 1'b1) ? {{20{1'b1}},Instr[31:20]} :  // consider sign extension of most signifiacnt bit ie. bit 31 here
                    (ImmSel == 3'b010 & Instr[31] == 1'b0) ? {{20{1'b0}},Instr[31:25],Instr[11:7]} : // S type immediate 
                    (ImmSel == 3'b010 & Instr[31] == 1'b1) ? {{20{1'b1}},Instr[31:25],Instr[11:7]} : 
-                   (ImmSel == 3'b011 & Instr[31] == 1'b0) ? {{20{1'b0}},Instr[31],Instr[7],Instr[30:25],Instr[11:8]} : // B type immediate 
-                   (ImmSel == 3'b011 & Instr[31] == 1'b1) ? {{20{1'b1}},Instr[31],Instr[7],Instr[30:25],Instr[11:8]} : 
+                   (ImmSel == 3'b011 & Instr[31] == 1'b0) ? {{19{1'b0}},Instr[31],Instr[7],Instr[30:25],Instr[11:8],1'b0} : // B type immediate 
+                   (ImmSel == 3'b011 & Instr[31] == 1'b1) ? {{19{1'b1}},Instr[31],Instr[7],Instr[30:25],Instr[11:8],1'b0} : 
                    32'h00000000;
 
 endmodule
