8e24445c35246f2830a733406e8ed527c0c49a7b
MANAV SHAH <manavbshah28@gmail.com>
2025-12-23 06:44:50 -0500

    Decode Stage : modules init and connected.

--- pipe_RV32I/src/core.sv
+++ pipe_RV32I/src/core.sv
@@ -56,6 +56,14 @@ logic w_PC_Sel_M;
 
 logic [ILEN-1:0] w_instr_F;
 
+logic w_RegWEn;
+logic [XLEN-1:0] w_mux3_out_W;
+
+logic [XLEN-1:0] w_DataA;
+logic [XLEN-1:0] w_DataB;
+logic [XLEN-1:0] w_ImmSel_D;
+logic [XLEN-1:0] w_Immediate_D;
+
 // __________________________________________________________________________
 // Module Connections
 // __________________________________________________________________________
@@ -88,6 +96,25 @@ Instruction_memory imem(
 
 // DECODE
 
+Regfile regfile(
+    .clk(clk),
+    .rst(rst),
+    .WE(w_RegWEn),
+    .AddA(INSTR_reg_F[19:15]),
+    .AddB(INSTR_reg_F[24:20]),
+    .AddD(INSTR_reg_M[11:7]),
+    .DataD(w_mux3_out_W),
+    //
+    .DataA(w_DataA),
+    .DataB(w_DataB) 
+);
+
+Imm_Gen immediate_gen(
+    .Instr(INSTR_reg_F),
+    .ImmSel(w_ImmSel_D),
+    .immediate(w_Immediate_D)
+);
+
 // EXECUTE
 
 // MEMORY ACCESS
@@ -111,10 +138,42 @@ INSTR_reg_F <= w_instr_F;
 end
 end
 // DECODE
-
+always @(posedge clk or negedge rst)begin
+if(!rst)begin
+PC_reg_D <= 0;
+RD1_reg_D <= 0;
+RD2_reg_D <= 0;
+IMM_reg_D <= 0;
+INSTR_reg_D <= 0;
+end
+else begin
+PC_reg_D <= PC_reg_F;
+RD1_reg_D <= w_DataA;
+RD2_reg_D <= w_DataB;
+IMM_reg_D <= w_Immediate_D;
+INSTR_reg_D <= INSTR_reg_F;
+end
+end
 // EXECUTE
-
+always @(posedge clk or negedge rst)begin
+if(!rst)begin
+end
+else begin
+end
+end
 // MEMORY ACCESS
-
+always @(posedge clk or negedge rst)begin
+if(!rst)begin
+end
+else begin
+end
+end
 // WRITE BACK
+always @(posedge clk or negedge rst)begin
+if(!rst)begin
+end
+else begin
+end
+end
+
 endmodule
